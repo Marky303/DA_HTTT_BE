df565fbf17af19178ba40187ddf1f2b5b5c0e1dd
NhienHuu <nhienhuu303@gmail.com>
2024-10-30 00:41:15 +0700

    Minor prompt changes

--- analysis/gemini/QueryGraphAnalysis.py
+++ analysis/gemini/QueryGraphAnalysis.py
@@ -11,13 +11,14 @@ class Graph(Enum):
     BAR = 'bar'
     LINE = 'line'
     PIE = 'pie'
+    NONE = 'none'
 
 def QueryPostgresDatamart(query: str, graphType: Graph, graphName: str):
-    """Create a Postgres query as parameter to query the infomation asked by the user in the Postgres datamart. The function will search the postgres database using the query and return the user the table result. After that, the function will draw a graph based on the graphType parameter. For bar graph and pie graph, the first column of the result in the query MUST be categories or labels (the x axis). For line graph, the first column is the x axis.
+    """Create a Postgres query as parameter to query the infomation asked by the user in the Postgres datamart. The function will search the postgres database using the query and return the user the table result. After that, the function will draw a graph based on the graphType parameter. For bar graph and pie graph, the first column of the result in the query MUST be categories or labels (the x axis). For line graph, the first column (usually a date column) is the x axis.
     Args:
         query: Create a query that will be queried in the Postgres datamart. The query must be on a single line. Only generate the syntatically correct and bare query, without any newline or other special characters. You can rename the columns of the query result to match the content. 
-        graphType: The type of graph that the function will draw after querying the result. graphType is of enum(string) type. 
-        graphName: The name of the graph that the function will draw.
+        graphType: The type of graph that the function will draw after querying the result. graphType is of enum(string) type. ONLY CHOOSE "none" WHEN THE DATA CANNOT BE DESCRIBED IN THESE GRAPHS.
+        graphName: The name of the graph that the function will draw. If the graphType is "none", the graph name will also be "none".
     Returns:
         The result of the query (and maybe a graph)
     """
